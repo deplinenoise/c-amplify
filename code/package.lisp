f8ad953e8bc7eb0004c6cee0460793893d5aec8c
dep <deplinenoise@gmail.com>
2010-03-08 18:30:03 -0800

    Fixed package/symbol shadowing reload bug.

--- code/package.lisp
+++ code/package.lisp
@@ -5,6 +5,11 @@
   (:use :common-lisp)
   (:import-from :cl-match match))
 
+(eval-when (:compile-toplevel)
+  (let ((sym-pkg (find-package '#:se.defmacro.c-amplify.csym)))
+    (when sym-pkg
+      (delete-package sym-pkg))))
+
 (defpackage #:se.defmacro.c-amplify.csym
   (:use)
   (:documentation 
